01d97e65ddb07c96bc01f12714d6963d3dd57f64
jagrutijoshi95 <joshijagruti95@gmail.com>
2023-03-14 03:34:05 +0530

    Added Unit Test cases

---
+++ assets/mocks/repository.mock.ts
@@ -0,0 +1,5 @@
+export const repositoryMock =
+    [
+        { id: "160138471", name: 'testRepo1', description: "", topics: ["Angular", "JS"] },
+        { id: "465464545", name: 'testRepo2', description: "", topics: ["JS"] }
+    ];
