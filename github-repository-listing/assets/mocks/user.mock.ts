f23b942d08c05394ccd1d619659e6aecb22ee497
jagrutijoshi95 <joshijagruti95@gmail.com>
2023-03-14 01:28:41 +0530

    initial commit

---
+++ github-repository-listing/assets/mocks/user.mock.ts
@@ -0,0 +1,12 @@
+export const userMock =
+{
+    "login": "jagrutijoshi95",
+    "id": 29602182,
+    "avatar_url": "https://avatars.githubusercontent.com/u/29602182?v=4",
+    "html_url": "https://github.com/jagrutijoshi95",
+    "name": "Jagruti Joshi",
+    "location": "Gujarat",
+    "bio": "",
+    "twitter_username": "",
+    "public_repos": 10
+}
